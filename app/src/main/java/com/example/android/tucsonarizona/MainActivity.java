268a21db9cb6765977a562a2aca2b1c3cee9b843
Vincent William Hunt <35615333+vincentwilliamhunt@users.noreply.github.com>
2018-03-21 17:44:47 -0700

    Add files via upload

--- app/src/main/java/com/example/android/tucsonarizona/MainActivity.java
+++ app/src/main/java/com/example/android/tucsonarizona/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.tucsonarizona;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,17 +9,23 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int score = 0;
-
+    /**
+     * This app calculates score for the quiz using methods
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    /**
+     * This method determines if question one is correct and returns one point
+     * if the correct RadioButton is checked
+     */
     public int QuestionOne() {
         RadioButton threeFifty = (RadioButton) findViewById(R.id.three_fifty);
         boolean isChecked = threeFifty.isChecked();
@@ -30,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * This method checks to see if the user has input "saguaro"
+     * This method uses .toLowerCase() and .Trim() to ignore trailing spaces and cap letters
+     * returns one point if the user enters the correct cactus
+     */
     public int QuestionTwo() {
         EditText cactus = (EditText) findViewById(R.id.cactus);
         String inputCactus = cactus.getText().toString();
@@ -46,6 +58,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * This method checks to see that the correct CheckBoxes are selected and returns one point
+     * if so.
+     */
     public int QuestionThree() {
         CheckBox gilaMonster = (CheckBox) findViewById(R.id.gila_monster);
         boolean gilaMonsterIsChecked = gilaMonster.isChecked();
@@ -62,6 +78,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * This method checks to see if the correct radioButton is checked and returns 1 point if true
+     */
     public int QuestionFour() {
         RadioButton cleanAir = (RadioButton) findViewById(R.id.clean_air);
         boolean cleanAirIsChecked = cleanAir.isChecked();
@@ -74,10 +93,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * This method sums four integers and returns the result
+     */
     public int CalculateScore(int point1, int point2, int point3, int point4) {
         return point1 + point2 + point3 + point4;
     }
 
+    /**
+     * This method submits the quiz when the submit button is checked.
+     * This method calls the defined modules above which allows it to display the score to the user
+     * in both a text view on the screen and a SHORT toast view.
+     */
     public void SubmitQuiz(View view) {
 
         int questionOnePoint = QuestionOne();
@@ -92,6 +119,10 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreTextView = (TextView) findViewById(R.id.score_text_view);
         scoreTextView.setText("" + totalScore + "/4");
 
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        CharSequence text = getString(R.string.toast_one) + " " + totalScore + " " +getString(R.string.toast_two);
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
-
 }
